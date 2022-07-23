471becbaa213813aa085a1b32f5da5902b737d66
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2022-07-23 00:16:52 -0900

    Login Process is now functional, but without session or localstorage

--- NeonNotesOnline/Controllers/AuthController.cs
+++ NeonNotesOnline/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
 ﻿using NeonNotesOnline.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,7 +26,61 @@ namespace NeonNotesOnline.Controllers
 
         public ActionResult LoginProcess(LoginData lgdata)
         {
-            return Content(String.Format("Email: {0} Password: {1}", lgdata.email, lgdata.password));
+
+            List<Register> accountStore = new List<Register>();
+
+            using (DBModel getAccount = new DBModel())
+            {
+                var data = getAccount.AccountsTables.Where(x => x.email == lgdata.email && x.password == lgdata.password).FirstOrDefault();
+                if (data != null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            //return Content(String.Format("Email: {0} Password: {1}", lgdata.email, lgdata.password));
+        }
+
+        public ActionResult RegisterProcess(Register regdata)
+        {
+            regdata.userName = userNameGenerator(regdata.lastName);
+
+            var newAccountTable = new AccountsTable()
+            {
+                userName = regdata.userName,
+                firstName = regdata.firstName,
+                lastName = regdata.lastName,
+                email = regdata.email,
+                password = regdata.password
+            };
+
+            try
+            {
+                using (DBModel newAccount = new DBModel())
+                {
+                    newAccount.AccountsTables.Add(newAccountTable);
+                    newAccount.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                return RedirectToAction("Register");
+                //return Content("Error!"+ ex.Message);
+            }
+            //return Content(String.Format("Firstname: {0} Lastname: {1} Email: {2} Password: {3}", regdata.firstName, regdata.lastName, regdata.email, regdata.password));
+        }
+
+        public string userNameGenerator(string fname)
+        {
+            Random rnd = new Random();
+            string userNameTest = String.Format("{0}_{1}", fname , rnd.Next(1000, 9999).ToString());
+            string refurbishString = userNameTest.Replace(" ", "");
+            return refurbishString;
         }
     }
 }
---
+++ NeonNotesOnline/Models/Register.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NeonNotesOnline.Models
+{
+    public class Register
+    {
+        [Required]
+        public string userName { get; set; }
+        [Required]
+        public string firstName { get; set; }
+        [Required]
+        public string lastName { get; set; }
+        [Required]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid Email")]
+        public string email { get; set; }
+        [Required]
+        public string password { get; set; }
+    }
+}
